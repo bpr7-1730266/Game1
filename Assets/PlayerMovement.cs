8904ead3e8eff0615119d966ed0c23c1d58cf7a1
bpr7-1730266 <37883986+bpr7-1730266@users.noreply.github.com>
2020-03-11 17:20:45 +0100

    first commit
    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float speed = 50f;
+    public bool Flipper;
+    void Update()
+    {
+        if (Flipper && Input.GetKey(KeyCode.A))
+        { GetComponent<HingeJoint2D>().useMotor = true; }
+
+        else
+        { GetComponent<HingeJoint2D>().useMotor = false; }
+    }
+
+
+
+    // Update is called once per frame
+    /*void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Letter A hit");
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+
+    }*/
+
+}
+
